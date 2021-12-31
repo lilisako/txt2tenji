7a52126f2dd5290c147ccac86c95a4f51c76e4d5
risako <lilisako062397@gmail.com>
2021-12-31 11:26:55 +0900

    first commit

---
+++ lib/txt2tenji.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "txt2tenji/version"
+
+module Txt2tenji
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/txt2tenji_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Txt2tenji do
+  it "has a version number" do
+    expect(Txt2tenji::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
